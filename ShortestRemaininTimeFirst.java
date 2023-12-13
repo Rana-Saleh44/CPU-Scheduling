13f1b46fef20ffd31b4f4cbfdbf7ef77881f2adf
fatma242 <fatmaessam395@gmail.com>
2023-12-13 22:30:55 +0200

    updated

--- ShortestRemaininTimeFirst.java
+++ ShortestRemaininTimeFirst.java
@@ -4,13 +4,14 @@ import java.util.Collections;
 public class ShortestRemaininTimeFirst {
     private Vector<Process> pVector;                      // for Entering processes
     private Vector<Process> pVector2;                     // for complete processes
-    private Vector<Process> res;                          // for result schedule
+    private Vector<Process> outputVector;                  // for output processes => remBursttime != 0
+    private int starvation = 10;
     private double AvgWaitingTime;
     private double AvgTurnaroundTime;
     public ShortestRemaininTimeFirst(){
         this.pVector = new Vector<>();
         this.pVector2 = new Vector<>();
-        this.res = new Vector<>();
+        this.outputVector = new Vector<>();
         this.AvgWaitingTime = 0.0;
         this.AvgTurnaroundTime = 0.0;
     } 
@@ -25,7 +26,7 @@ public class ShortestRemaininTimeFirst {
         }
         return true;
     }
-    public Process LEASTBurstTime(int currenttime){
+    private Process LEASTRemainingBurstTime(int currenttime){
         int MinimumBurstTime = 1000;
         int index = 0;
         for(int i = 0; i < this.pVector.size() && this.pVector.get(i).getArrivalTime() <= currenttime; i++){
@@ -36,37 +37,81 @@ public class ShortestRemaininTimeFirst {
         }
         return this.pVector.get(index);
     }
-    private int getIndex(Process p){
-        return this.pVector.indexOf(p);
-   }
+    private Process getMaxPriority(){
+        int max = 0;
+        int index = 0;
+        for(int i = 0; i < this.pVector.size(); i++){
+            if(this.pVector.get(i).getPriorityNumber() > max){
+                max = this.pVector.get(i).getPriorityNumber();
+                index = i;
+            }
+        }
+        return this.pVector.get(index);
+    }
+    
     public void RunSRTF(){
         Process p;
         int index;
         int time;
+        int flag = 0;
         Collections.sort(pVector);
         for(time  = this.pVector.get(0).getArrivalTime(); !this.terminated(); ){
-            p = this.LEASTBurstTime(time);
-            index = this.getIndex(p);
-            if(this.pVector.get(index).remBurstTime != 0){
+            Process p2 = this.getMaxPriority();                                     // getmaxpriority to solve starvation                            
+            p = this.LEASTRemainingBurstTime(time);                                 // current process = p
+            if(p2.getPriorityNumber() > 0){
+                flag = 1;
+                index = this.pVector.indexOf(p2);
                 this.pVector.get(index).StartTime = time;
-            }
-            while (p.remBurstTime > 0) {
-                Process q = this.LEASTBurstTime(time);
-                if(!q.getID().equals(p.getID())){
-                    break;
+                while (p2.remBurstTime > 0) {
+                    Process q = this.LEASTRemainingBurstTime(time);                   // nextprocess = q
+                    if(!q.getID().equals(p2.getID())){
+                        this.outputVector.add(p2);
+                        break;
+                    }
+                    p2.remBurstTime--;
+                    time++;
                 }
-                p.remBurstTime--;
-                time++;
+                for(Process process: outputVector){
+                    process.WaitingTime++;
+                    if(process.WaitingTime >= starvation){
+                        process.setPriorityNumber(process.getPriorityNumber() + 1);
+                    }
+                }
+            }
+            else{
+                flag = 0;
+                index = this.pVector.indexOf(p);
+                this.pVector.get(index).StartTime = time;
+                while (p.remBurstTime > 0) {
+                    Process q = this.LEASTRemainingBurstTime(time);               // nextprocess = q
+                    if(!q.getID().equals(p.getID())){
+                        this.outputVector.add(p);
+                        break;
+                    }
+                    p.remBurstTime--;
+                    time++;
+                }
+                for(Process process: outputVector){
+                    process.WaitingTime++;
+                    if(process.WaitingTime >= starvation){
+                        process.setPriorityNumber(process.getPriorityNumber() + 1);
+                    }
+                }
             }
             this.pVector.get(index).WaitingTime += this.pVector.get(index).StartTime - this.pVector.get(index).getArrivalTime();
             this.pVector.get(index).TurnaroundTime = this.pVector.get(index).WaitingTime + this.pVector.get(index).getBurstTime() - this.pVector.get(index).remBurstTime;
             System.out.println("Time: "+ this.pVector.get(index).StartTime + " ::Process " + this.pVector.get(index).getID() + " => Waiting Time = "+ this.pVector.get(index).WaitingTime + ",  TurnAround Time = "+ this.pVector.get(index).TurnaroundTime);
             this.pVector.get(index).setArrivalTime(time);
             if(pVector.get(index).remBurstTime == 0){
-                this.pVector2.add(p);
-                this.pVector.remove(p);
+                if(flag == 1){
+                    this.pVector2.add(p2);
+                    this.pVector.remove(p2);
+                }
+                else{
+                    this.pVector2.add(p);
+                    this.pVector.remove(p);
+                }
             }
-            res.add(p);
         }
     }
     public void Printing(){
