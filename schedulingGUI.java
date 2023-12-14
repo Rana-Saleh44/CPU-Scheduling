ef2354c259e544b0681582061be78e906cf04541
fatma242 <fatmaessam395@gmail.com>
2023-12-14 14:56:11 +0200

    updated

--- schedulingGUI.java
+++ schedulingGUI.java
@@ -2,7 +2,6 @@ import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-import java.util.ArrayList;
 import java.util.Vector;
 
 import javax.swing.*;
